ef8b457845fbdee14a42151f48cd40d1582ef702
bazbalderas <2430204@upv.edu.mx>
2025-11-05 06:58:28 -0600

    Se agrego una documentación y también se agrego en el incio un usuario por defecto para poder inciar sesión

--- Práctica 9/Registro/registro.js
+++ Práctica 9/Registro/registro.js
@@ -1,3 +1,4 @@
+//Traemos los IDs necesarios para poder hacer funcionar el registro (me falló todo)
 const loginFormulario = document.getElementById('loginFormulario');
 
 const name = document.getElementById('nombre').value;
@@ -7,30 +8,35 @@ const user = document.getElementById('usuario').value;
 const pass = document.getElementById('contraseña').value;
 const conf_pass = document.getElementById('conf_contraseña').value;
 
-loginFormulario.addEventListener('submit', function(event){
+//Al clickear el botón de enviar inician las funciones del evento
+loginFormulario.addEventListener('registrar', function(event){
     event.preventDefault();
 
+    //Si la contraseña es menor a 5 caracteres
 if(pass.lenght < 5){
     alert("La contraseña debe ser mayor a 5 caracteres");
     return;
 }
 
+//SI la contraseña y la confirmación de contraseña es diferente
 if(pass !== conf_pass){
     alert("Las contraseñas deben coincidir");
     return;
 }
 
+//Función para validar que el email tenga la nomenclatura correcta
 function validarEmail(email) {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
 }
 
+//VAlidar que el email sea válido
 if(!validarEmail(email)){
     alert("El correo electrónico no es válido");
     return;
 }
 
-// Inicializar arreglo de usuarios
+// Inicializar arreglo de usuarios para ir agregandolos, hacerles push al localStorage y enviar un mensaje antes de redireccionarlos hacia el inicio de sesion
 let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
 const newUser = {
